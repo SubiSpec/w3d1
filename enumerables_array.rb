e2f6f5074e24c35a08d7c24ffe63c1cb5017725b
Rafael Campos <rafaelcampos04@hotmail.com>
2024-03-18 16:11:25 -0400

    worked on my select

--- enumerables_array.rb
+++ enumerables_array.rb
@@ -2,14 +2,25 @@ class Array
     def my_each(&prc)
         self.map { |ele| prc.call(ele) }
     end
+
+    def my_select(&prc)
+
+        result = []
+
+        self.my_each(prc).each do |ele|
+            if prc.call(ele)
+                result << ele
+            end
+        end
+    end
+
 end
 
 return_value = [1, 2, 3].my_each do |num|
     puts num
-end
-self.my_each do |num|
+   end.my_each do |num|
     puts num
-end
+   end
    # => 1
    #    2
    #    3
@@ -17,4 +28,9 @@ end
    #    2
    #    3
    
-p return_value  # => [1, 2, 3]
+   p return_value  # => [1, 2, 3]
+
+
+# a = [1, 2, 3]
+# a.my_select { |num| num > 1 } # => [2, 3]
+# a.my_select { |num| num == 4 } # => []
